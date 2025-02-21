d05a576d4fc707798403aabe6b978c5f5696954c
Karl.Chen <k010165pid@gmail.com>
2025-02-21 16:13:00 +0800

    總複習之一

---
+++ GoodStore/Controllers/ProductListController.cs
@@ -0,0 +1,21 @@
+﻿using GoodStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodStore.Controllers
+{
+    public class ProductListController : Controller
+    {
+        private readonly GoodStoreContext _context;
+        public ProductListController(GoodStoreContext goodStore)
+        {
+            _context = goodStore;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var p = await _context.Product.ToListAsync();
+            return View(p);
+        }
+    }
+}
